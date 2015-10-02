5ba5bdfd24d285624fa590c272bdcf82e3beff52
Bryan Smith <BryanRSmith@gmail.com>
2015-10-01 22:17:07 -0700

    add karma firefox launcher

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,9 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    plugins : [
+      'karma-firefox-launcher'
+    ],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
