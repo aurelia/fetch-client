f87cf6198392f468eb5b88984960edf30db13f00
Bryan Smith <BryanRSmith@gmail.com>
2015-10-02 19:32:20 -0700

    update travis config

--- karma.conf.js
+++ karma.conf.js
@@ -74,7 +74,7 @@ module.exports = function(config) {
         base: 'Chrome',
         flags: ['--no-sandbox']
       }
-    }
+    },
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
