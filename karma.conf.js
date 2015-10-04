3b94c5d3744a0608f88e7a54a0c4ded8173efcfe
Bryan Smith <BryanRSmith@gmail.com>
2015-10-04 14:23:58 -0700

    test(all): load fetch polyfill with karma

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-    files: [],
+    files: ['node_modules/whatwg-fetch/fetch.js'],
 
 
     // list of files to exclude
