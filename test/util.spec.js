8c32f8ed2a99b9d0f17117773dcb8d0fcf8168b9
Bryan Smith <BryanRSmith@gmail.com>
2015-11-30 13:20:33 -0800

    test(util): simplify test

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,45 +1,20 @@
-import {HttpClient} from '../src/http-client';
 import {json} from '../src/util';
 
 describe('util', () => {
-  let originalFetch = window.fetch;
-  let client;
-  let fetch;
-
-  function setUpTests() {
-    beforeEach(() => {
-      client = new HttpClient();
-      fetch = window.fetch = jasmine.createSpy('fetch');
-    });
-
-    afterEach(() => {
-      fetch = window.fetch = originalFetch;
-    });
-  }
-
-  setUpTests();
-
   describe('json', () => {
-    it('sets JSON body and Content-Type header on Requests', (done) => {
-      fetch.and.returnValue(emptyResponse(200));
-      const data = { test: 'data' };
+    it('creates JSON blobs', (done) => {
+      let data = { test: 'data' };
+      let blob = json(data);
 
-      client.fetch('path', {
-        method: 'POST',
-        body: json(data)
-      }).then(() => {
-        let [request] = fetch.calls.first().args;
-        expect(request).toEqual(jasmine.any(Request));
-        expect(request.headers.get('Content-Type')).toBe('application/json');
-        return request.json();
-      }).then(bodyContent => {
-        expect(bodyContent).toEqual(data);
+      expect(blob.type).toEqual('application/json');
+
+      let reader = new FileReader();
+      reader.addEventListener('loadend', () => {
+        expect(JSON.parse(reader.result)).toEqual(data);
         done();
       });
+
+      reader.readAsText(blob);
     });
   });
 });
-
-function emptyResponse(status) {
-  return Promise.resolve(new Response(null, { status }));
-}
