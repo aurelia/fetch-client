a24364217cc2fa913cd7537e6f30639580be400f
Bryan Smith <BryanRSmith@gmail.com>
2015-11-30 20:38:05 -0800

    break something

--- src/http-client.js
+++ src/http-client.js
@@ -37,7 +37,7 @@ export class HttpClient {
   */
   interceptors: Interceptor[] = [];
 
-  constructor() {
+  constructor(){
     if (typeof fetch === 'undefined') {
       throw new Error('HttpClient requires a Fetch API implementation, but the current environment doesn\'t support it. You may need to load a polyfill such as https://github.com/github/fetch.');
     }
